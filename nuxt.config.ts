e84b1873c1bf696d874fe10526bf8cd140733d73
Tiago Martins <tiago@cbpf.br>
2023-12-07 16:50:36 -0300

    🎉 Inicio da minha página

---
+++ nuxt.config.ts
@@ -0,0 +1,47 @@
+export default defineNuxtConfig({
+	css: ['~/assets/global.sass'],
+	modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxtjs/google-fonts', '@nuxtjs/i18n', '@vueuse/nuxt', 'nuxt-icon', '@nuxtseo/module', '@nuxt/image', 'nuxt-primevue'],
+	app: {
+		head: {
+			templateParams: {
+			  separator: '•',
+			},
+		  },
+	},
+	colorMode: {
+		preference: 'dark',
+		fallback: 'dark',
+		classSuffix: '',
+	},
+	googleFonts: {
+		families: {
+			'Montserrat+Alternates': true,
+			'Delius+Unicase': true,
+		},
+	},
+	i18n: {
+		baseUrl: 'https://martinsds.dev.br/',
+		defaultLocale: 'pt',
+		langDir: 'locales',
+		locales: [
+			{ code: 'en', iso: 'en-US', name: 'English (US)', file: 'en.ts' },
+			{ code: 'es', iso: 'es', name: 'Español (ESP)', file: 'es.ts' },
+			{ code: 'pt', iso: 'pt-BR', name: 'Português (BR)', file: 'pt.ts' },
+		],
+	},
+	site: {
+		url: 'https://martinsds.dev.br/',
+		name: 'Tiago Martins',
+		description: 'Desenvolvedor Front-End',
+		defaultLocale: 'pt',
+		identity: {
+			type: 'Person',
+		},
+	},
+	linkChecker: {
+		enabled: false,
+	},
+	devtools: {
+		enabled: true,
+	},
+})
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<section class="bg-red-300">
+		<h1>Oi Chego em breve</h1>
+	</section>
+</template>
